5340ebd50f567c6e28ea6e1d75c6e3a07d2b3d96
Carjardying <carolinejardy.pro@gmail.com>
2025-02-12 12:39:06 +0100

    étape 1 ok

---
+++ index.js
@@ -0,0 +1,17 @@
+function demandeUtilisateur (){
+    let demandeNumero = (prompt("Choisis un nombre"));
+    let givenNumber = parseInt(demandeNumero);
+    return givenNumber;
+}
+/* givenNumber(22); */
+/* function didIwin (givenNumber){
+    if (givenNumber < 22) {
+        alert("Plus grand");
+    } else if (givenNumber > 22) {
+        alert("Plus petit");
+    } else {
+        alert("Bravo! Vous avez deviné le nombre");
+    }
+} */
+/* didIwin(givenNumber); */
+
