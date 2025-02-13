15e2b23b2242a8e23414f5c3acb60216fba296dc
Carjardying <carolinejardy.pro@gmail.com>
2025-02-13 10:42:24 +0100

    Etapes 2/2 et  3 ok

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-let givenNumber = parseInt(prompt("Choisis un nombre"));
+//on demande à l'utilisateur de choisir un nombre
+function askNumber(){
+    return parseInt(prompt("Choisis un nombre")); //parseInt sur le prompt pour pas avoir à le faire la ligne en dessous
+}
+let givenNumber = askNumber(); //on enregistre le resultat du askNumber dans une variable pour l'appleer ensuite 
   
   
 function didIwin (givenNumber){
@@ -13,4 +17,17 @@ function didIwin (givenNumber){
         return false;
     }
 }
-didIwin(givenNumber); 
+didIwin(givenNumber); //on appele la fonction pour voir 1fois. 
+//je rajoute false et true sur les resultats, plus petit, plus grand et égalité pour la prochaine fonction
+function gamePlay (){ 
+    if (didIwin(givenNumber) == false) {
+        givenNumber = askNumber();
+        gamePlay();
+    }
+/*     while(!didIwin(givenNumber)){ //!givenNumber = "givenNumber = false" mais en plus simple
+        givenNumber = askNumber();
+    } */
+}
+
+gamePlay() //gameplay lance didIwin tant que givenNumber n'est pas true
+
